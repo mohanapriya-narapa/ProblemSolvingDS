f3ec68a192c09d29030adbeac7eac5e5ce55b20d
mohanapriya-narapa <mohanapriya.narapa@outlook.com>
2024-09-23 20:06:04 -0500

    is unique

---
+++ ProblemSolving/src/Strings/Unique.java
@@ -0,0 +1,24 @@
+package Strings;
+
+public class Unique {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(isUnique("mango"));
+	}
+
+	private static boolean isUnique(String str) {
+		// TODO Auto-generated method stub
+		boolean[] arr=new boolean[128];
+		for(int i=0;i<str.length();i++)
+		{
+			if(arr[str.charAt(i)])
+			{
+				return false;
+			}
+			arr[str.charAt(i)]=true;
+		}
+		return true;
+	}
+
+}
