ae04fc11a0f04fff71a45a5c740e5229f8cd91e7
mohanapriya-narapa <mohanapriya.narapa@outlook.com>
2024-09-23 23:52:15 -0500

    palindrome permutation

---
+++ ProblemSolving/src/Strings/PalindromePermutation.java
@@ -0,0 +1,35 @@
+package Strings;
+
+public class PalindromePermutation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(palindromePermutation("tacat"));
+	}
+
+	private static boolean palindromePermutation(String str) {
+		// TODO Auto-generated method stub
+		int[] arr =new int[128];
+		int count=0;
+		for(int i=0;i<str.length();i++)
+		{
+			if(str.charAt(i)!=32)
+			{
+				arr[str.charAt(i)]++;
+			}
+			
+		}
+		for(int i1=0;i1<arr.length;i1++)
+		{
+			
+			if(arr[i1]>0 && (arr[i1]%2!=0))
+			{
+				count++;
+			}
+		}
+		if(count>1)
+			return false;
+		return true;
+	}
+
+}
