ffbea062d7c5c0f238b3ba970c0e4292ba5f113a
mohanapriya-narapa <mohanapriya.narapa@outlook.com>
2024-09-23 20:32:58 -0500

    check permutation

---
+++ ProblemSolving/src/Strings/CheckPermutation.java
@@ -0,0 +1,31 @@
+package Strings;
+
+public class CheckPermutation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println(checkPermutation("dog", "god"));
+	}
+
+	private static boolean checkPermutation(String str1, String str2) {
+		// TODO Auto-generated method stub
+		int[] arr =new int[128];
+		for(int i=0;i<str1.length();i++)
+		{
+			arr[str1.charAt(i)]++;
+		}
+		for(int i=0;i<str2.length();i++)
+		{
+			arr[str2.charAt(i)]--;
+		}
+		for(int i=0;i<arr.length;i++)
+		{
+			if(arr[i]!=0)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
